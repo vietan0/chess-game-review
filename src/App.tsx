d955340979f1d4d53857badc64f3bd44125bac15
Viet An <tiktaktictac269@gmail.com>
2024-08-18 16:41:27 +0700

    feat: add sound effects to moves

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { Icon } from '@iconify/react/dist/iconify.js';
 import { Button } from '@nextui-org/button';
 import { Chess } from 'chess.js';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useShallow } from 'zustand/react/shallow';
 
 import Board from './Board';
 import PGNForm from './PGNForm';
+import capture from './sound/capture.mp3';
+import castle from './sound/castle.mp3';
+import moveCheck from './sound/move-check.mp3';
+import moveSelf from './sound/move-self.mp3';
+import promote from './sound/promote.mp3';
 import { useStore } from './store';
+
+const sound: Record<string, HTMLAudioElement> = {
+  moveSelf: new Audio(moveSelf),
+  moveCheck: new Audio(moveCheck),
+  castle: new Audio(castle),
+  capture: new Audio(capture),
+  promote: new Audio(promote),
+};
 
 export default function App() {
   const {
@@ -34,6 +47,58 @@ export default function App() {
   const history = currentGame.history({ verbose: true });
   const displayedGame = new Chess(currentMoveNum > 0 ? history[currentMoveNum - 1].after : undefined);
   const [_randomState, setRandomState] = useState(0); // to trigger re-render when PGNForm call currentGame.loadPgn()
+  const prevMove = currentMoveNum === 0 ? undefined : history[currentMoveNum - 1];
+
+  useEffect(() => {
+    function stopAll() {
+      for (const key in sound) {
+        sound[key].pause();
+        sound[key].currentTime = 0;
+      }
+    }
+
+    function playSound() {
+      if (!prevMove) {
+        // starting position
+        stopAll();
+      }
+
+      else {
+        if (new Chess(prevMove.after).inCheck()) {
+          stopAll();
+          sound.moveCheck.play();
+
+          return;
+        }
+
+        if (prevMove.flags.includes('p')) {
+          stopAll();
+          sound.promote.play();
+
+          return;
+        }
+
+        if (prevMove.flags.includes('c') || prevMove.flags.includes('e')) {
+          stopAll();
+          sound.capture.play();
+
+          return;
+        }
+
+        if (prevMove.flags.includes('k') || prevMove.flags.includes('q')) {
+          stopAll();
+          sound.castle.play();
+        }
+
+        else {
+          stopAll();
+          sound.moveSelf.play();
+        }
+      }
+    }
+
+    playSound();
+  }, [currentMoveNum]);
 
   return (
     <div className="mx-auto flex max-w-7xl justify-center gap-8 p-6" id="App">
@@ -60,6 +125,7 @@ export default function App() {
           <Button
             aria-label="First move"
             className="grow text-3xl"
+            disableRipple
             isDisabled={history.length === 0 || currentMoveNum === 0}
             isIconOnly
             onPress={toFirstMove}
@@ -70,6 +136,7 @@ export default function App() {
           <Button
             aria-label="Previous move"
             className="grow text-3xl"
+            disableRipple
             isDisabled={history.length === 0 || currentMoveNum === 0}
             isIconOnly
             onPress={toPrevMove}
@@ -80,6 +147,7 @@ export default function App() {
           <Button
             aria-label="Next move"
             className="grow text-3xl"
+            disableRipple
             isDisabled={history.length === 0 || currentMoveNum === history.length}
             isIconOnly
             onPress={toNextMove}
@@ -90,6 +158,7 @@ export default function App() {
           <Button
             aria-label="Last move"
             className="grow text-3xl"
+            disableRipple
             isDisabled={history.length === 0 || currentMoveNum === history.length}
             isIconOnly
             onPress={toLastMove}
