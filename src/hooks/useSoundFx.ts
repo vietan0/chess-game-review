cd5abfe4b2ca11a0d67ee666bfe9a8dbf7e81b05
Viet An <tiktaktictac269@gmail.com>
2024-10-14 16:55:54 +0700

    fix: play correct sound effect when navigating backwards

--- src/hooks/useSoundFx.ts
+++ src/hooks/useSoundFx.ts
@@ -10,6 +10,8 @@ import moveSelf from '../sounds/move-self.mp3';
 import promote from '../sounds/promote.mp3';
 import { useBoardStore } from '../stores/useBoardStore';
 
+import type { Move } from 'chess.js';
+
 export default function useSoundFx() {
   const [playMoveSelf] = useSound(moveSelf);
   const [playMoveCheck] = useSound(moveCheck);
@@ -31,45 +33,44 @@ export default function useSoundFx() {
   const history = currentGame.history({ verbose: true });
   const currentMove = history[currentMoveNum - 1];
 
+  function chooseSound(move: Move) {
+    if (new Chess(move.after).inCheck()) {
+      playMoveCheck();
+
+      return;
+    }
+
+    if (move.flags.includes('p')) {
+      playPromote();
+
+      return;
+    }
+
+    if (move.flags.includes('c') || move.flags.includes('e')) {
+      playCapture();
+
+      return;
+    }
+
+    if (move.flags.includes('k') || move.flags.includes('q')) {
+      playCastle();
+    }
+
+    else {
+      playMoveSelf();
+    }
+  }
+
   useEffect(() => {
-    function playSound() {
-      if (!currentMove) {
-        // starting position
-
-        if (lastNav === -1) {
-          playMoveSelf();
-        }
-      }
-
-      else {
-        if (new Chess(currentMove.after).inCheck()) {
-          playMoveCheck();
-
-          return;
-        }
-
-        if (currentMove.flags.includes('p')) {
-          playPromote();
-
-          return;
-        }
-
-        if (currentMove.flags.includes('c') || currentMove.flags.includes('e')) {
-          playCapture();
-
-          return;
-        }
-
-        if (currentMove.flags.includes('k') || currentMove.flags.includes('q')) {
-          playCastle();
-        }
-
-        else {
-          playMoveSelf();
-        }
+    if (!currentMove) {
+      if (lastNav === -1) {
+        playMoveSelf();
       }
     }
 
-    playSound();
+    else {
+      const nextMove = history[currentMoveNum];
+      chooseSound(lastNav === -1 ? nextMove : currentMove);
+    }
   }, [currentMoveNum]);
 }
