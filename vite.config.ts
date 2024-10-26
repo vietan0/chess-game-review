1d186948a18ef06ec80eddb615e82089a21d364b
Viet An <tiktaktictac269@gmail.com>
2024-10-26 17:53:33 +0700

    refactor: get openings from another source - https://github.com/lichess-org/chess-openings/

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import dsv from '@rollup/plugin-dsv';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { configDefaults } from 'vitest/config';
-
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dsv()],
   server: {
     open: true,
     headers: {
