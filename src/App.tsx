7936d206da50623972af6f3e101c9f625335ddc0
Viet An <tiktaktictac269@gmail.com>
2024-11-19 16:54:29 +0700

    feat: add top-level error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@nextui-org/button';
 import { useEffect } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import Board from './components/board/Board';
 import Controls from './components/controls/Controls';
@@ -11,9 +13,32 @@ export default function App() {
   }, []);
 
   return (
-    <div className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-6 p-6 lg:h-full lg:flex-row lg:items-stretch" id="App">
-      <Board />
-      <Controls />
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <div className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-6 p-6 lg:h-full lg:flex-row lg:items-stretch" id="App">
+        <Board />
+        <Controls />
+      </div>
+    </ErrorBoundary>
+  );
+}
+
+// eslint-disable-next-line unused-imports/no-unused-vars
+function Fallback({ error, resetErrorBoundary }: { error: Error; resetErrorBoundary: () => any }) {
+  return (
+    <div className="flex h-full flex-col gap-2 p-8" role="alert">
+      <p className="text-2xl">Something went wrong:</p>
+      <code className="inline-block text-small text-danger-500">
+        {error.message}
+      </code>
+      <div className="flex gap-1">
+        <Button
+          onPress={() => {
+            window.location.reload();
+          }}
+        >
+          Refresh
+        </Button>
+      </div>
     </div>
   );
 }
